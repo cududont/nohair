0ef6a6c7bfe500cb921a86e026789429f6f88aa2
cududont <45621973+cududont@users.noreply.github.com>
2019-02-05 21:59:08 +0200

    Add files via upload

--- nohair.py
+++ nohair.py
@@ -1,20 +1,42 @@
 from tkinter import*
+from pynput.keyboard import Key, Listener
+from threading import Thread
 
-root = Tk()
+def show():
+    global label, root
+    print("On")
+    root = Tk()
+    sw = root.winfo_screenwidth()
+    sh = root.winfo_screenheight()
+    dsw = str(int(sw / 2))
+    dsh = str(int(sh / 2))
+    label = Label(root, text="Test", font=('Arial','30'), fg='black', bg='white')
+    label.master.overrideredirect(True)
+    label.master.lift()
+    label.master.wm_attributes("-topmost", True)
+    label.master.wm_attributes("-disabled", True)
+    label.master.wm_attributes("-transparentcolor", "white")
+    label.pack()
+    label.master.geometry("+"+dsw+"+"+dsh)
+    root.mainloop()
 
-sw = root.winfo_screenwidth()
-sh = root.winfo_screenheight()
-dsw = str(int(sw / 2))
-dsh = str(int(sh / 2))
+
+def remove():
+    print("Off")
+    label.destroy()
+    
+    
+
+def released(key):
+    if key == Key.up:
+         thread = Thread(target=show)
+         thread.start()
+    elif key == Key.down:
+        remove()
+              
+
+with Listener(on_release=released) as listener:
+    listener.join()
 
 
-label = Label(text="+", font=('Arial','30'), fg='black', bg='white')
-label.master.overrideredirect(True)
-label.master.geometry("+"+dsw+"+"+dsh)
-label.master.lift()
-label.master.wm_attributes("-topmost", True)
-label.master.wm_attributes("-disabled", True)
-label.master.wm_attributes("-transparentcolor", "white")
-label.pack()
 
-label.mainloop()
